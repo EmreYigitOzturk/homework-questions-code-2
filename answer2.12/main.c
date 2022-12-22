7e5b7974fea90fadc719f9968d25cc96df466bbb
EmreYigitOzturk <118299119+EmreYigitOzturk@users.noreply.github.com>
2022-12-22 15:34:08 +0300

    Add files via upload

---
+++ answer2.12/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int s=0,i,n,sum=0,x;
+    printf("enter number\n");
+    scanf("%d",&x);
+    for(n=1;n<=x;)
+    {
+        s=0;
+        for(i=1;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                s+=i;
+            }
+        }
+        if(n==s)
+        {
+            printf("%d\n",s);
+        }
+        n++;
+    }
+
+
+
+
+    return 0;
+}
